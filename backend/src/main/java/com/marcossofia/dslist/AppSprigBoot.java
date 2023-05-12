c4812bf267368c434bbc6760edcf35120c85cff7
Marcos M Santos <marcos1970moreirasantos@gmail.com>
2023-05-12 08:53:09 -0300

    Creatw

---
+++ backend/src/main/java/com/marcossofia/dslist/AppSprigBoot.java
@@ -0,0 +1,13 @@
+package com.marcossofia.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class AppSprigBoot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSprigBoot.class, args);
+
+	}
+
+}
---
+++ backend/src/test/java/com/marcossofia/dslist/DslistApplicationGameTests.java
@@ -0,0 +1,13 @@
+package com.marcossofia.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationGameTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
